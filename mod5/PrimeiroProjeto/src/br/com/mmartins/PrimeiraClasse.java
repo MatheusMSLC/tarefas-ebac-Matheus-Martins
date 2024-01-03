86fbd344db2d89fd93e1d2df5c23260e0374eddc
MatheusMSLC <155547500+MatheusMSLC@users.noreply.github.com>
2024-01-03 13:04:45 -0300

    mod5

---
+++ mod5/PrimeiroProjeto/src/br/com/mmartins/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package br.com.mmartins;
+
+public class PrimeiraClasse {
+    public static void main(String[] args){
+        String nome = args[0];
+        String sobrenome = args[1];
+        System.out.print("Hello " + nome + " " + sobrenome);
+    }
+
+}
