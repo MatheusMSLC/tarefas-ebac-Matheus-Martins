a05ed68e05faf90bdafd75c3e8a814b1b7632dd0
MatheusMSLC <155547500+MatheusMSLC@users.noreply.github.com>
2024-01-03 13:14:16 -0300

    Primeira Tarefa

---
+++ mod5/PrimeiroProjeto/PrimeiraClasse.java
@@ -0,0 +1,7 @@
+public class PrimeiraClasse {
+    public static void main(String[] args){
+        String nome = args[0];
+        String sobrenome = args[1];
+        System.out.print("Hello " + nome + " " + sobrenome);
+    }
+}
